4024044c7ddc034209be8f70fd2376d6fbb6da0c
nimrod02 <letrun.a@gmail.com>
2024-01-03 10:45:51 +0100

    add task 0 and readme

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+def canUnlockAll(boxes):
+        """Determines if all the boxes can be opened.
+        Args:
+                boxes (list): list of lists of integers.
+        
+        Returns:
+                True if all boxes can be opened, else return False.
+        """
+        if len(boxes) == 0:
+                return False
+        if len(boxes) == 1:
+                return True
+        keys = [0]
+        for key in keys:
+                for box in boxes[key]:
+                        if box not in keys and box < len(boxes):
+                                keys.append(box)
+        if len(keys) == len(boxes):
+                return True
+        return False
