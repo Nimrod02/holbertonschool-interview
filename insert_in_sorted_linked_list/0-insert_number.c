289ce1597fd2a0b6d80018fd627f01364040bfad
nimrod02 <letrun.a@gmail.com>
2024-01-12 14:55:39 +0100

    fix complexity

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -10,28 +10,28 @@
 listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new_node = malloc(sizeof(listint_t));
-    listint_t *current = *head, *prev = NULL;
-
     if (new_node == NULL)
         return (NULL);
 
     new_node->n = number;
+    new_node->next = NULL;
 
-    while (current != NULL && current->n < number)
-    {
-        prev = current;
-        current = current->next;
-    }
-
-    if (prev == NULL)
+    // Case 1: Insert at the beginning or if the list is empty
+    if (*head == NULL || (*head)->n >= number)
     {
         new_node->next = *head;
         *head = new_node;
     }
     else
     {
-        new_node->next = prev->next;
-        prev->next = new_node;
+        // Case 2: Traverse the list to find the insertion point
+        listint_t *current = *head;
+        while (current->next != NULL && current->next->n < number)
+        {
+            current = current->next;
+        }
+        new_node->next = current->next;
+        current->next = new_node;
     }
 
     return (new_node);
