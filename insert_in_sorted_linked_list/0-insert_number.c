a825fed5b064ebb2bf046a500cfb21bcfa16f896
nimrod02 <letrun.a@gmail.com>
2024-01-12 14:41:36 +0100

    fix task 0

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -9,26 +9,28 @@
  */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new;
-	listint_t *current;
+    listint_t *new;
+    listint_t *current = *head;
 
-	current = *head;
+    new = malloc(sizeof(listint_t));
+    if (new == NULL)
+        return (NULL);
 
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
+    new->n = number;
+    new->next = NULL;
 
-	new->n = number;
-	new->next = NULL;
-
-	if (*head == NULL)
-		*head = new;
-	else
-	{
-		while (current->next != NULL)
-			current = current->next;
-		current->next = new;
-	}
-	
-	return (new);
+    if (*head == NULL || (*head)->n >= number)
+    {
+        new->next = *head;
+        *head = new;
+    }
+    else
+    {
+        while (current->next != NULL && current->next->n < number)
+            current = current->next;
+        new->next = current->next;
+        current->next = new;
+    }
+    
+    return (new);
 }
