1483c5b662cc798e8803d49bfd0d63a97357bb9b
nimrod02 <letrun.a@gmail.com>
2024-01-12 14:34:32 +0100

    add lib

--- insert_in_sorted_linked_list/lists.h
+++ insert_in_sorted_linked_list/lists.h
@@ -2,6 +2,7 @@
 #define LISTS_H
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -23,4 +24,4 @@ void free_listint(listint_t *head);
 
 listint_t *insert_node(listint_t **head, int number);
 
-#endif /* LISTS_H */
+#endif
