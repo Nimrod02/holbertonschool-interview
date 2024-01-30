79966fd253c55206d05d0ea8224659a9490152c2
nimrod02 <letrun.a@gmail.com>
2024-01-30 10:17:36 +0100

    0-sandpiles.c with sandpiles_sum func

---
+++ sandpiles/0-sandpiles.c
@@ -0,0 +1,32 @@
+#include "sandpiles.h"
+
+/**
+ * sandpiles_sum - Sum 3x3 grids
+ * @grid1: Left 3x3 grid
+ * @grid2: Right 3x3 grid
+ *
+ */
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+    int i, j;
+
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid1[i][j]);
+        }
+
+        printf(" %c ", (i == 1 ? '+' : ' '));
+
+        for (j = 0; j < 3; j++)
+        {
+            if (j)
+                printf(" ");
+            printf("%d", grid2[i][j]);
+        }
+        printf("\n");
+    }
+}
