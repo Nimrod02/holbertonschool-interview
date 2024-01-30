234e3d3431d79005e795e9461cbc9193f05f8c67
nimrod02 <letrun.a@gmail.com>
2024-01-30 10:20:22 +0100

    add header file with all prototype and lib used

---
+++ sandpiles/sandpiles.h
@@ -0,0 +1,10 @@
+#ifndef __SANDPILES_H__
+#define __SANDPILES_H__
+#include <stdlib.h>
+#include <stdio.h>
+
+void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid_sum(int grid1[3][3], int grid2[3][3]);
+void print_grid(int grid[3][3]);
+
+#endif
