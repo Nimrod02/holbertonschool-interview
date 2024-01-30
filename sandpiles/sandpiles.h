3f13b04d3ad0fbab8fe899e488e62bd3d8395330
nimrod02 <letrun.a@gmail.com>
2024-01-30 10:23:21 +0100

    fix header file

--- sandpiles/sandpiles.h
+++ sandpiles/sandpiles.h
@@ -1,10 +1,8 @@
-#ifndef __SANDPILES_H__
-#define __SANDPILES_H__
-#include <stdlib.h>
+#ifndef SANDPILES_H
+#define SANDPILES_H
 #include <stdio.h>
+#include <stdlib.h>
 
 void sandpiles_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid_sum(int grid1[3][3], int grid2[3][3]);
-void print_grid(int grid[3][3]);
 
 #endif
