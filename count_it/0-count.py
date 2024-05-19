9c47e31bf1b8fbb6fd100b40faa0b614e6ae5a0b
nimrod02 <letrun.a@gmail.com>
2024-05-20 00:24:24 +0200

    refactor the cause of linter

--- count_it/0-count.py
+++ count_it/0-count.py
@@ -17,33 +17,24 @@ def count_words(subreddit, word_list):
         None
     """
 
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {
-        'User-Agent': 'Python/requests:APIproject:0.0.1 (by /u/veeteeran)'
-    }
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = \
+        {'User-Agent': 'Python/requests:APIproject:0.0.1 (by /u/veeteeran)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
         return None
 
-    hot_posts = response.json().get('data', {}).get('children', [])
-    hot_titles = [post.get('data', {}).get('title', '') for post in hot_posts]
+    hot_posts = response.json().get('data').get('children')
+    hot_titles = [post.get('data').get('title') for post in hot_posts]
 
     word_count = {}
     for word in word_list:
-        word_count[word.lower()] = 0
+        word_count[word] = 0
+        for title in hot_titles:
+            word_count[word] += len(re.findall(
+                r'\b{}\b'.format(word), title, re.IGNORECASE))
 
-    for title in hot_titles:
-        for word in word_list:
-            word_lower = word.lower()
-            word_count[word_lower] += len(
-                re.findall(
-                    r'\b{}\b'.format(re.escape(word_lower)),
-                    title,
-                    re.IGNORECASE
-                )
-            )
-
-    for word in sorted(word_count, key=lambda x: (-word_count[x], x)):
+    for word in sorted(word_list, key=lambda x: (-word_count[x], x)):
         if word_count[word] > 0:
-            print(f'{word}: {word_count[word]}')
+            print('{}: {}'.format(word.lower(), word_count[word]))
