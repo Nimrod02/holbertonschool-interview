c4731cef0ecdd1e0db69cb719b7869022e7f6ed2
nimrod02 <letrun.a@gmail.com>
2024-06-04 14:33:52 +0200

    add header file

---
+++ infinite_multiplication/holberton.h
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+
+int is_number(char *str);
+int multiply(int num1, int num2);
+int _putchar(char c);
+
+
+#endif /* HOLBERTON_H */
