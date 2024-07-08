423e9d8d559c62dabdf47e874543e5eade86f102
nimrod02 <letrun.a@gmail.com>
2024-07-08 08:35:17 +0200

    refactor again

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -19,11 +19,16 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
-
-    for coin in coins:
-        for x in range(coin, total + 1):
-            dp[x] = min(dp[x], dp[x - coin] + 1)
-
-    return dp[total] if dp[total] != float('inf') else -1
+    coins.sort(reverse=True)
+    sum = 0
+    i = 0
+    counter = 0
+    num_coins = len(coins)
+    while sum < total and i < num_coins:
+        while coins[i] <= total - sum:
+            sum += coins[i]
+            counter += 1
+            if sum == total:
+                return counter
+        i += 1
+    return -1
