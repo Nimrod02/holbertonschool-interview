edafb7c6e5ccbeed514dbc0199cf58793788af78
nimrod02 <letrun.a@gmail.com>
2024-07-08 08:17:53 +0200

    fix

--- making_change/0-making_change.py
+++ making_change/0-making_change.py
@@ -4,30 +4,26 @@ making_change module
 """
 
 
-def make_change(coins, total):
-    """
-    Given a pile of coins of different values, determine the fewest number of coins needed to meet a given amount total.
+def makeChange(coins, total):
+    """ Given a pile of coins of different values,
+    determine the fewest number of coins needed to meet a given amount total
 
     Args:
-        coins (list of int): List of the values of the coins in your possession.
-        total (int): The total amount of money you need to make change for.
+        coins (list of int): list of the values of the coins in your possession
+        total (int): the total amount of money you need to make change for
 
     Returns:
-        int: The fewest number of coins needed to meet the given amount total, or -1 if it's not possible.
+        int: return the fewest number of coins needed to meet
+        a given amount total
     """
-    coins.sort(reverse=True)
+    if total <= 0:
+        return 0
 
-    current_sum = 0
-    coin_count = 0
-    i = 0
-    num_coins = len(coins)
+    dp = [float('inf')] * (total + 1)
+    dp[0] = 0
 
-    while current_sum < total and i < num_coins:
-        while coins[i] <= total - current_sum:
-            current_sum += coins[i]
-            coin_count += 1
-            if current_sum == total:
-                return coin_count
-        i += 1
+    for coin in coins:
+        for x in range(coin, total + 1):
+            dp[x] = min(dp[x], dp[x - coin] + 1)
 
-    return -1
+    return dp[total] if dp[total] != float('inf') else -1
