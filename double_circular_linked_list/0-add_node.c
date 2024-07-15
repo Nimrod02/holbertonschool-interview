5bfd82df9d54ef4fa8e80eb28373c4e24d8061fc
nimrod02 <letrun.a@gmail.com>
2024-07-15 09:15:27 +0200

    fix linter standar error for header file and task 0

--- double_circular_linked_list/0-add_node.c
+++ double_circular_linked_list/0-add_node.c
@@ -12,6 +12,7 @@
 List *add_node_end(List **list, char *str)
 {
 	List *new_node = malloc(sizeof(List));
+
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
@@ -48,6 +49,7 @@ List *add_node_end(List **list, char *str)
 List *add_node_begin(List **list, char *str)
 {
 	List *new_node = malloc(sizeof(List));
+
 	if (!new_node)
 		return (NULL);
 	new_node->str = strdup(str);
