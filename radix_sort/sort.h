b35cb01f38eab1e94634af2bc8c44114da706446
nimrod02 <letrun.a@gmail.com>
2024-07-22 17:50:47 +0200

    forget to push the updated header file

--- radix_sort/sort.h
+++ radix_sort/sort.h
@@ -5,6 +5,7 @@
 #include <stdio.h>
 
 void radix_sort(int *array, size_t size);
+void count_sort_LSD(int *array, size_t size, size_t lsd);
 void print_array(const int *array, size_t size);
 
 #endif /* SORT_H */
