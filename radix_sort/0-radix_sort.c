9c2b7ace03dbee3dc7aa6420a1f8ba6fa63dc79f
nimrod02 <letrun.a@gmail.com>
2024-07-22 17:47:13 +0200

    refactor the code with a new function to match with specific cases

--- radix_sort/0-radix_sort.c
+++ radix_sort/0-radix_sort.c
@@ -1,47 +1,56 @@
 #include "sort.h"
 
-
 /**
- * radix_sort - sorts an array of integers using the Radix sort algorithm
- * @array: array to be sorted
+ * radix_sort - sorts an array following the Radix sort algorithm
+ * @array: array of ints to sort
  * @size: size of the array
  */
 void radix_sort(int *array, size_t size)
 {
-	int i, exp = 1, max = 0;
-	int *tmp;
-
-	tmp = malloc(sizeof(int) * size);
-
-	if (!tmp)
+	if (!array || size < 2)
 		return;
 
-	for (i = 0; i < (int)size; i++)
-	{
+	int max = array[0];
+
+	for (size_t i = 1; i < size; i++)
 		if (array[i] > max)
 			max = array[i];
-	}
 
-	while (max / exp > 0)
+	for (size_t lsd = 1; max / lsd > 0; lsd *= 10)
 	{
-		int bucket[10] = {0};
-
-		for (i = 0; i < (int)size; i++)
-			bucket[(array[i] / exp) % 10]++;
-
-		for (i = 1; i < 10; i++)
-			bucket[i] += bucket[i - 1];
-
-		for (i = size - 1; i >= 0; i--)
-			tmp[--bucket[(array[i] / exp) % 10]] = array[i];
-
-		for (i = 0; i < (int)size; i++)
-			array[i] = tmp[i];
-
-		exp *= 10;
-
+		count_sort_LSD(array, size, lsd);
 		print_array(array, size);
 	}
+}
 
-	free(tmp);
+/**
+ * count_sort_LSD - count sort with LSD
+ * @array: array to sort
+ * @size: size of the array
+ * @lsd: least significant digit
+ */
+void count_sort_LSD(int *array, size_t size, size_t lsd)
+{
+	int count[10] = {0};
+	int *output = malloc(sizeof(int) * size);
+
+	if (!output)
+		return;
+
+	for (size_t i = 0; i < size; i++)
+		count[(array[i] / lsd) % 10]++;
+
+	for (int i = 1; i < 10; i++)
+		count[i] += count[i - 1];
+
+	for (int i = size - 1; i >= 0; i--)
+	{
+		output[count[(array[i] / lsd) % 10] - 1] = array[i];
+		count[(array[i] / lsd) % 10]--;
+	}
+
+	for (size_t i = 0; i < size; i++)
+		array[i] = output[i];
+
+	free(output);
 }
