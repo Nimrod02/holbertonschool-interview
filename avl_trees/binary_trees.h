3bf11d8d6c669015b3290c23a0ad19c469d76e87
nimrod02 <letrun.a@gmail.com>
2024-08-05 12:47:50 +0200

    fix header file for the complete compile of the checker

--- avl_trees/binary_trees.h
+++ avl_trees/binary_trees.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
  * struct binary_tree_s - Binary tree node
  *
@@ -23,23 +24,8 @@ struct binary_tree_s
 
 typedef struct binary_tree_s binary_tree_t;
 
-/**
- * struct binary_tree_s - Binary tree node
- *
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-typedef struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-} binary_tree_t;
-
 void binary_tree_print(const binary_tree_t *);
 int binary_tree_is_avl(const binary_tree_t *tree);
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 #endif /* BINARY_TREES_H */
